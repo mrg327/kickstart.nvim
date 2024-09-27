16efb3d0b19ea156bcd652ba739705f6c73670eb
Matthew Ghere <extern.matthew.ghere1@vw.com>
2024-09-27 10:52:07 -0700

    Docstring snippet

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -1,28 +1,36 @@
-local ls = require('luasnip')  -- Require the luasnip module
+local ls = require('luasnip')  -- Load the luasnip module
+local fmt = require('luasnip.extras.fmt').fmt
 local s = ls.snippet
-local t = ls.text_node
 local i = ls.insert_node
 
--- Define a simple snippet for a 'for' loop in Python
+-- Define a snippet for a Python docstring template using fmt
 ls.add_snippets('python', {
-  s('docstr', {
-    t('\'\'\''),
-    i(1, 'Description'),
-    t({'', '', 'Argument(s)'}),
-    t({'', '-----------'}),
-    t({'', ''}),
-    i(2, 'argument1'),
-    t(' -- '),
-    i(3, 'description1'),
-    t(''),
-    t({'', 'Returns'}),
-    t({'', '-----------'}),
-    t({'', ''}),
-    i(4, 'return1'),
-    t(' -- '),
-    i(5, 'ret_description1'),
-    t({''}),
-    t('\'\'\''),
-  })
+  s('docstr', fmt([[
+    '''
+    {}
+    
+    Argument(s)
+    -----------
+    {} -- {}
+
+    Returns
+    -----------
+    {} -- {}
+    '''
+  ]], {
+    i(1, 'Description'),       -- Placeholder for the description
+    i(2, 'argument1'),         -- Placeholder for the argument name
+    i(3, 'description1'),      -- Placeholder for the argument description
+    i(4, 'return1'),           -- Placeholder for the return value
+    i(5, 'ret_description1')   -- Placeholder for the return description
+  }))
 })
 
+ls.add_snippets('python', {
+  s('dpara', fmt([[
+{} -- {}
+  ]],{
+    i(1, 'param'),
+    i(2, 'description')
+  }))
+})
