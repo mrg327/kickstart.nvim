732065f4dff9ea44fdb89df963107242954f74c6
Matthew Ghere <extern.matthew.ghere1@vw.com>
2024-12-04 10:56:05 -0800

    Add minigit to the mini nvim plugins

--- init.lua
+++ init.lua
@@ -915,6 +915,9 @@ require('lazy').setup(
         -- - sr)'  - [S]urround [R]eplace [)] [']
         require('mini.surround').setup()
 
+        -- Use MiniGit for git related actions
+        require('mini.git').setup()
+
         -- Simple and easy statusline.
         --  You could remove this setup call if you don't like it,
         --  and try some other statusline plugin
