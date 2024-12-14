c2cc076d8fb9b2787694981062a515d7311f626f
Matthew Ghere <matthewghere327@gmail.com>
2024-12-13 23:26:49 -0700

    Add comments to dir_import file

--- dir_import.lua
+++ dir_import.lua
@@ -1,3 +1,12 @@
+--[[
+This will tell git you want to start ignoring the changes to the file
+git update-index --assume-unchanged path/to/file
+
+When you want to start keeping track again
+git update-index --no-assume-unchanged path/to/file
+--]]
+
+
 local configs = {pyright = "C:/Users/DVUHPQU/AppData/Roaming/npm/pyright-langserver",
                  python = "C:/Users/DVUHPQU/.venv/Scripts/python.exe"}
 
