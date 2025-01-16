0ba5f145fd6e21783b5bf515ae9ef5bd64eb36ae
dvuszir <extern.matthew.ghere@vw.com>
2025-01-16 10:56:31 -0800

    Add cmp fmt colot gitsigns treesitter and whichkey to modules

--- init.lua
+++ init.lua
@@ -107,7 +107,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false 
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -257,22 +257,20 @@ require('lazy').setup(
     -- with the first argument being the link and the following
     -- keys can be used to configure plugin behavior/loading/etc.
     --
-    -- Use `opts = {}` to force a plugin to be loaded.
-    --
-
-    -- Here is a more advanced example where we pass configuration
-    -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-    --    require('gitsigns').setup({ ... })
-    --
 
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins.debug' },
   { import = 'custom.plugins.mini' },
   { import = 'custom.plugins.gitsigns' },
+  { import = 'custom.plugins.colorscheme' },
   { import = 'custom.plugins.oil' },
   { import = 'custom.plugins.vimtex' },
   { import = 'custom.plugins.telescope' },
   { import = 'custom.plugins.lsp' },
+  { import = 'custom.plugins.autocomplete' },
+  { import = 'custom.plugins.autoformat' },
+  { import = 'custom.plugins.whichkey' },
+  { import = 'custom.plugins.treesitter' },
 
     -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
     --
@@ -288,61 +286,6 @@ require('lazy').setup(
     -- Then, because we use the `config` key, the configuration only runs
     -- after the plugin has been loaded:
     --  config = function() ... end
-
-    { -- Useful plugin to show you pending keybinds.
-      'folke/which-key.nvim',
-      event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-      opts = {
-        icons = {
-          -- set icon mappings to true if you have a Nerd Font
-          mappings = vim.g.have_nerd_font,
-          -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-          -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
-          keys = vim.g.have_nerd_font and {} or {
-            Up = '<Up> ',
-            Down = '<Down> ',
-            Left = '<Left> ',
-            Right = '<Right> ',
-            C = '<C-…> ',
-            M = '<M-…> ',
-            D = '<D-…> ',
-            S = '<S-…> ',
-            CR = '<CR> ',
-            Esc = '<Esc> ',
-            ScrollWheelDown = '<ScrollWheelDown> ',
-            ScrollWheelUp = '<ScrollWheelUp> ',
-            NL = '<NL> ',
-            BS = '<BS> ',
-            Space = '<Space> ',
-            Tab = '<Tab> ',
-            F1 = '<F1>',
-            F2 = '<F2>',
-            F3 = '<F3>',
-            F4 = '<F4>',
-            F5 = '<F5>',
-            F6 = '<F6>',
-            F7 = '<F7>',
-            F8 = '<F8>',
-            F9 = '<F9>',
-            F10 = '<F10>',
-            F11 = '<F11>',
-            F12 = '<F12>',
-          },
-        },
-
-        -- Document existing key chains
-        spec = {
-          { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-          { '<leader>d', group = '[D]ebug' },
-          { '<leader>p', group = '[L]SP' },
-          { '<leader>r', group = '[R]ename' },
-          { '<leader>s', group = '[S]earch' },
-          { '<leader>w', group = '[W]orkspace' },
-          { '<leader>t', group = '[T]oggle' },
-          { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-        },
-      },
-    },
 
     -- NOTE: Plugins can specify dependencies.
     --
@@ -366,220 +309,12 @@ require('lazy').setup(
       },
     },
     { 'Bilal2453/luvit-meta', lazy = true },
-    
-
-    { -- Autoformat
-      'stevearc/conform.nvim',
-      event = { 'BufWritePre' },
-      cmd = { 'ConformInfo' },
-      keys = {
-        {
-          '<leader>f',
-          function()
-            require('conform').format { async = true, lsp_format = 'fallback' }
-          end,
-          mode = '',
-          desc = '[F]ormat buffer',
-        },
-      },
-      opts = {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for languages that don't
-          -- have a well standardized coding style. You can add additional
-          -- languages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true }
-          local lsp_format_opt
-          if disable_filetypes[vim.bo[bufnr].filetype] then
-            lsp_format_opt = 'never'
-          else
-            lsp_format_opt = 'fallback'
-          end
-          return {
-            timeout_ms = 3000,
-            lsp_format = lsp_format_opt,
-          }
-        end,
-        formatters_by_ft = {
-          lua = { 'ast-grep', 'stylua', stop_after_first = true },
-          -- Conform can also run multiple formatters sequentially
-          python = { 'black', stop_after_first = true },
-          --
-          markdown = { 'mdformat', 'markdownlint', stop_after_first = true },
-          -- You can use 'stop_after_first' to run the first available formatter from the list
-          -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        },
-      },
-    },
-
-    { -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      event = 'InsertEnter',
-      dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
-        {
-          'L3MON4D3/LuaSnip',
-          build = (function()
-            -- Build Step is needed for regex support in snippets.
-            -- This step is not supported in many windows environments.
-            -- Remove the below condition to re-enable on windows.
-            if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-              return
-            end
-            return 'make install_jsregexp'
-          end)(),
-          dependencies = {
-            -- `friendly-snippets` contains a variety of premade snippets.
-            --    See the README about individual language/framework/plugin snippets:
-            --    https://github.com/rafamadriz/friendly-snippets
-            {
-              'rafamadriz/friendly-snippets',
-              config = function()
-                require('luasnip.loaders.from_vscode').lazy_load()
-              end,
-            },
-          },
-        },
-        'saadparwaiz1/cmp_luasnip',
-
-        -- Adds other completion capabilities.
-        --  nvim-cmp does not ship with all sources by default. They are split
-        --  into multiple repos for maintenance purposes.
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-path',
-      },
-      config = function()
-        -- Require custiom snippets
-        require 'custom.snippets.lua'
-        require 'custom.snippets.python'
-        require 'custom.snippets.latex'
-
-        -- See `:help cmp`
-        local cmp = require 'cmp'
-        local luasnip = require 'luasnip'
-        luasnip.config.setup {}
-
-        cmp.setup {
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-          completion = { completeopt = 'menu,menuone,noinsert' },
-
-          -- For an understanding of why these mappings were
-          -- chosen, you will need to read `:help ins-completion`
-          --
-          -- No, but seriously. Please read `:help ins-completion`, it is really good!
-          mapping = cmp.mapping.preset.insert {
-            -- Select the [n]ext item
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            -- Select the [p]revious item
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-            -- Scroll the documentation window [b]ack / [f]orward
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-            -- Accept ([y]es) the completion.
-            --  This will auto-import if your LSP supports it.
-            --  This will expand snippets if the LSP sent a snippet.
-            ['<tab>'] = cmp.mapping.confirm { select = true },
-
-            -- If you prefer more traditional completion keymaps,
-            -- you can uncomment the following lines
-            --['<CR>'] = cmp.mapping.confirm { select = true },
-            --['<Tab>'] = cmp.mapping.select_next_item(),
-            --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-            -- Manually trigger a completion from nvim-cmp.
-            --  Generally you don't need this, because nvim-cmp will display
-            --  completions whenever it has completion options available.
-            ['<C-Space>'] = cmp.mapping.complete {},
-
-            -- Think of <c-l> as moving to the right of your snippet expansion.
-            --  So if you have a snippet that's like:
-            --  function $name($args)
-            --    $body
-            --  end
-            --
-            -- <c-l> will move you to the right of each of the expansion locations.
-            -- <c-h> is similar, except moving you backwards.
-            ['<C-l>'] = cmp.mapping(function()
-              if luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-              end
-            end, { 'i', 's' }),
-            ['<C-h>'] = cmp.mapping(function()
-              if luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-              end
-            end, { 'i', 's' }),
-
-            -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-            --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-          },
-          sources = {
-            {
-              name = 'lazydev',
-              -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-              group_index = 0,
-            },
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-            { name = 'path' },
-          },
-        }
-      end,
-    },
-
-    { -- You can easily change to a different colorscheme.
-      -- Change the name of the colorscheme plugin below, and then
-      -- change the command in the config to whatever the name of that colorscheme is.
-      --
-      -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-      'folke/tokyonight.nvim',
-      priority = 1000, -- Make sure to load this before all the other start plugins.
-      init = function()
-        -- Load the colorscheme here.
-        -- Like many other themes, this one has different styles, and you could load
-        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'tokyonight-night'
-
-        -- You can configure highlights by doing something like:
-        vim.cmd.hi 'Comment gui=none'
-      end,
-    },
 
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-    
 
-    { -- Highlight, edit, and navigate code
-      'nvim-treesitter/nvim-treesitter',
-      build = ':TSUpdate',
-      main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter` opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
+    
 
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
@@ -602,7 +337,7 @@ require('lazy').setup(
   --    This is the easiest way to modularize your config.
   --
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  {
+  }, {
     ui = {
       -- If you are using a Nerd Font: set icons to an empty table which will use the
       -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
