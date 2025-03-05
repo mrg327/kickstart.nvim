cdbffdd6743e2fd14424239d5a53bc43dc6011ab
Matthew Ghere <extern.matthew.ghere1@vw.com>
2025-03-05 15:15:33 -0800

    Add ability to delete from the qflist

--- init.lua
+++ init.lua
@@ -106,6 +106,8 @@ end
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.g.python3_host_prog = ENV_PATHS['python']
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false 
 
@@ -187,6 +189,19 @@ vim.keymap.set('n', '<leader>qi', vim.diagnostic.setloclist, { desc = 'Open diag
 vim.keymap.set('n', '<leader>qt', function()
   vim.cmd('TodoQuickFix cwd=' .. vim.fn.expand("%:p:h"))
 end, { desc = 'Open diagnostic [Q]uickfix [T]odo list' })
+
+local del_qf_item = function()
+  local items = vim.fn.getqflist()
+  local line = vim.fn.line('.')
+  table.remove(items, line)
+  vim.fn.setqflist(items, "r")
+  vim.api.nvim_win_set_cursor(0, { line, 0 })
+end
+
+-- this is super basic. I might improve it at some point, but it's good enough for how little I use
+-- the QF list
+vim.keymap.set("n", "dd", del_qf_item, { silent = true, buffer = true, desc = "Remove entry from QF" })
+vim.keymap.set("v", "D", del_qf_item, { silent = true, buffer = true, desc = "Remove entry from QF" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
