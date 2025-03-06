6cd9e4bce089e8f8548f246305b04ca6a12aaf7a
Matthew Ghere <extern.matthew.ghere1@vw.com>
2025-03-06 14:58:54 -0800

    remove the tag search

--- lua/custom/plugins/todo.lua
+++ lua/custom/plugins/todo.lua
@@ -1,7 +1,1 @@
-return { 'folke/todo-comments.nvim', 
-  event = 'VimEnter',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-  },
-  opts = { signs = false } }
+return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
