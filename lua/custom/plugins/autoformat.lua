fa956bbf05160db45dfabf323c2ffe9be2423f1e
mrg327 <matthewghere327@gmail.com>
2025-03-19 22:03:50 -0700

    remove mdformat

--- lua/custom/plugins/autoformat.lua
+++ lua/custom/plugins/autoformat.lua
@@ -35,7 +35,7 @@ return { -- Autoformat
       -- Conform can also run multiple formatters sequentially
       python = { 'pylint', 'black', stop_after_first = true },
       --
-      markdown = { 'mdformat', 'markdownlint', stop_after_first = true },
+      markdown = { 'markdownlint', stop_after_first = true },
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
