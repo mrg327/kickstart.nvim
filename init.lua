01225aa247b320cb7953e86cf83a4349e9a0bd9e
Nanki Sahota <extern.nanki.sahota@vw.com>
2025-04-21 12:54:48 -0700

    add undotree on vim enter

--- init.lua
+++ init.lua
@@ -217,6 +217,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- UndoTreeKeybinds
+-- TODO: Move this into the plugin file
+vim.keymap.set('n', '<leader>U', '<cmd>UndotreeToggle<CR>', { desc = 'Move focus to the upper window' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
