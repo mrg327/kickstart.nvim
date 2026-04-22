e2be0fd85fff5a073d4651ac30cb78809518beb9
Matthew Ghere <matthewghere327@gmail.com>
2026-04-22 16:49:35 -0700

    Limit md render to only md files

--- lua/custom/plugins/markdown-render.lua
+++ lua/custom/plugins/markdown-render.lua
@@ -5,5 +5,8 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+        file_types = { 'markdown' },
+        completions = { lsp = { enabled = true } },
+    },
 }
